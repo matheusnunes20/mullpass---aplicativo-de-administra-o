a13880875d66436401c79d9a4187ceb560910fac
Mateus Nunes <mn006811@gmail.com>
2026-05-04 20:59:23 -0300

    fix: ssl postgres

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,8 +5,8 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     require: true,
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
