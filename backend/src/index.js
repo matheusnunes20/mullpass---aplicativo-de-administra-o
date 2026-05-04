e1108c914703f73c98544bfa8658137a31dcdc59
Mateus Nunes <mn006811@gmail.com>
2026-05-04 18:16:04 -0300

    atualização

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,41 +1,13 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cron from 'node-cron';
+import alunosRoutes from './routes/alunosRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import rachaRoutes from './routes/rachaRoutes.js';
+import aulasRoutes from './routes/aulasRoutes.js';
+import usuariosRoutes from './routes/usuariosRoutes.js';
+import presencasRoutes from './routes/presencasRoutes.js';
+import turmasRoutes from './routes/turmasRoutes.js';
+import inscricoesRoutes from './routes/inscricoesRoutes.js';
+import financeiroRoutes from './routes/financeiroRoutes.js';
 
-import alunosRoutes from '../routes/alunosRoutes.js';
-import authRoutes from '../routes/authRoutes.js';
-import rachaRoutes from '../routes/rachaRoutes.js';
-import aulasRoutes from '../routes/aulasRoutes.js';
-import usuariosRoutes from '../routes/usuariosRoutes.js';
-import presencasRoutes from '../routes/presencasRoutes.js';
-import turmasRoutes from '../routes/turmasRoutes.js';
-import inscricoesRoutes from '../routes/inscricoesRoutes.js';
-import financeiroRoutes from '../routes/financeiroRoutes.js';
-
-import { bloquearInadimplente } from '../middlewares/financeiroMiddleware.js';
-import { authMiddleware } from '../middlewares/authMiddleware.js';
-import { gerarMensalidades } from '../services/financeiroService.js';
-
-dotenv.config();
-
-const app = express();
-cron.schedule('0 2 * * *', () => {
-  gerarMensalidades();
-});
-app.use(cors({ origin: '*' }));
-app.use(express.json());
-app.use('/auth', authRoutes);
-app.use('/alunos', alunosRoutes);
-app.use('/usuarios', authMiddleware, usuariosRoutes);
-app.use('/turmas', authMiddleware, turmasRoutes);
-app.use('/inscricoes', authMiddleware, inscricoesRoutes);
-app.use('/financeiro', authMiddleware, financeiroRoutes);
-app.use('/presencas', authMiddleware, bloquearInadimplente, presencasRoutes);
-app.use('/rachas', authMiddleware, bloquearInadimplente, rachaRoutes);
-app.use('/aulas', authMiddleware, bloquearInadimplente, aulasRoutes);
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, '0.0.0.0', () => {
-});
+import { bloquearInadimplente } from './middlewares/financeiroMiddleware.js';
+import { authMiddleware } from './middlewares/authMiddleware.js';
+import { gerarMensalidades } from './services/financeiroService.js';
